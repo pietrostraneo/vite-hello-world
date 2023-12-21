13ecc2d4258ab0c94db57d39fa800fce23c2ae7a
pietrostraneo <straneo_p@yahoo.it>
2023-12-21 14:49:45 +0100

    Created and compiled AppTitle component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            message: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <h1>{{message}}</h1>
+    </div>
+</template>
+<style lang="">
+    
+</style>
